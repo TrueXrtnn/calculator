a8233895415f30c17b5d8c6d159455c717e5557d
true.xrstn <mr.chareev@mail.ru>
2023-04-19 21:29:06 +0800

    dz

--- src/main/java/com/example/calculator/CalculatorController.java
+++ src/main/java/com/example/calculator/CalculatorController.java
@@ -8,29 +8,35 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/calculator")
 public class CalculatorController {
-private final CalculatorService calculatorService;
-public CalculatorController(CalculatorService calculatorService){
-    this.calculatorService = calculatorService;
-}
+    private final CalculatorService calculatorService;
+
+    public CalculatorController(CalculatorService calculatorService) {
+        this.calculatorService = calculatorService;
+    }
+
     @GetMapping
-    public String welcomeCalculator(){
+    public String welcomeCalculator() {
         return calculatorService.welcomeCalculator();
     }
+
     @GetMapping("/plus")
-    public String plus( @RequestParam int num1 , @RequestParam int num2){
-    return calculatorService.plus(num1, num2);
+    public String plus(@RequestParam int num1, @RequestParam int num2) {
+        return calculatorService.plus(num1, num2);
     }
+
     @GetMapping("/minus")
-    public String minus(@RequestParam int num1 , @RequestParam int num2){
-        return calculatorService.minus(num1,num2);
+    public String minus(@RequestParam int num1, @RequestParam int num2) {
+        return calculatorService.minus(num1, num2);
     }
+
     @GetMapping("/multiply")
-    public String multiply(@RequestParam int num1 , @RequestParam int num2){
-        return calculatorService.multiply(num1,num2);
+    public String multiply(@RequestParam int num1, @RequestParam int num2) {
+        return calculatorService.multiply(num1, num2);
 
     }
+
     @GetMapping("/divide")
-    public String divide(@RequestParam int num1 , @RequestParam int num2){
-        return calculatorService.divide(num1,num2);
+    public String divide(@RequestParam int num1, @RequestParam int num2) {
+        return calculatorService.divide(num1, num2);
     }
 }
--- src/main/java/com/example/calculator/CalculatorService.java
+++ src/main/java/com/example/calculator/CalculatorService.java
@@ -4,28 +4,32 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class CalculatorService {
-    public String welcomeCalculator(){
+    public String welcomeCalculator() {
         return "<b>Добро пожаловать в калькулятор!</b>";
     }
-public String plus(int num1, int num2){
 
-         int sum = num1 + num2;
-         return num1 + "+" + num2 + "=" + sum;
+    public String plus(int num1, int num2) {
 
-}
+        int sum = num1 + num2;
+        return num1 + "+" + num2 + "=" + sum;
+
+    }
+
     public String minus(int num1, int num2) {
         int result = num1 - num2;
         return num1 + "-" + num2 + "=" + result;
     }
-    public String multiply(int num1, int num2){
-        int result = num1*num2;
-        return num1+"*"+num2+"="+result;
+
+    public String multiply(int num1, int num2) {
+        int result = num1 * num2;
+        return num1 + "*" + num2 + "=" + result;
     }
-    public String divide(int num1,int num2){
-        if (num2!=0){
-          int result = num1/num2;
-          return num1+"/"+num2+"="+result;
-    } else return "На ноль делить нельзя!";
+
+    public String divide(int num1, int num2) {
+        if (num2 != 0) {
+            int result = num1 / num2;
+            return num1 + "/" + num2 + "=" + result;
+        } else return "На ноль делить нельзя!";
     }
 }
 
